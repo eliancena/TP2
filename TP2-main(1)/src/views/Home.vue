7905e4928c662b18df80dce65dd036e8edb4a30c
eliancena <86085422+eliancena@users.noreply.github.com>
2021-06-20 01:04:45 -0300

    Add files via upload
    Le agregue las operaciones que hay que hacer con los montos y la obtención de porcentajes. Faltaría solamente pasar las propiedades.

---
+++ TP2-main(1)/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="menu">
+    <datos-usuario></datos-usuario>
+  </div>
+</template>
+
+<script>
+import DatosUsuario from "@/components/DatosUsuario.vue";
+
+export default {
+  name: "Menu",
+  components: {
+    DatosUsuario,
+  },
+};
+</script>
+
+<style>
+.menu {
+  width: 100%;
+}
+</style>
---
+++ TP2-main(1)/src/views/InfoPlazoFijo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="info-plazo-fijo">
+    <h2>Información del plazo fijo</h2>
+    <datos-plazo-fijo></datos-plazo-fijo>
+    <tabla-reinversion></tabla-reinversion>
+  </div>
+</template>
+
+<script>
+import DatosPlazoFijo from "@/components/DatosPlazoFijo.vue";
+import TablaReinversion from "@/components/TablaReinversion.vue";
+
+export default {
+  components: {
+    DatosPlazoFijo,
+    TablaReinversion,
+  },
+};
+</script>
